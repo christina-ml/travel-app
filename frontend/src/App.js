d08209a43e17b43ac581e83dfd56264711be2b3b
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-09 11:52:20 -0500

    added basic NavBar for our pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
 // COMPONENTS
+import NavBar from "./Components/NavBar";
 import DisplayAllPlaces from './Components/DisplayAllPlaces';
 import Home from './Components/Home';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path="/" element={ <Home />} />
         <Route path="/places" element={ <DisplayAllPlaces /> } />
