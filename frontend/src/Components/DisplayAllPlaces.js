809f518b94acc2bde9c31ff6caebbd636191fbb4
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-09 12:23:21 -0500

    made axios.get request to test that it works in DisplayAllPlaces

--- frontend/src/Components/DisplayAllPlaces.js
+++ frontend/src/Components/DisplayAllPlaces.js
@@ -1,4 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const DisplayAllPlaces = () => {
+    const [places, setPlaces] = useState([]);
+
+    const API = process.env.REACT_APP_API_URL;
+
+    /* We want it to run on load, but only once. */
+    useEffect(()=>{
+        console.log("Trigger");
+        axios.get(API + "/places")
+            .then((res)=>{
+                console.log(res);
+            })
+    }, [])
+
     return (
         <div>Displaying all places</div>
     )
